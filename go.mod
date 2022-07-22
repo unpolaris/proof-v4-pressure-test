9005e0327b60fe54f62f8467e4ba460541c3f0b4
qs <lqs@33.cn>
2022-07-22 11:05:35 +0800

    add time num

--- go.mod
+++ go.mod
@@ -2,3 +2,13 @@ module proof-v4-pressure-test
 
 go 1.17
 
+require github.com/unpolaris/proof-v4-go v1.0.1
+
+require (
+	github.com/antihax/optional v1.0.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/oauth2 v0.0.0-20220630143837-2104d58473e0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -11,12 +11,14 @@ import (
 )
 
 var (
-	addr = flag.String("a", "", "the remote addr")
-	num  = flag.String("n", "", "the number of simulated users")
+	addr    = flag.String("a", "", "the remote addr")
+	num     = flag.String("n", "", "the number of simulated users")
+	timeNum = flag.String("t", "", "the number of time")
 )
 
-var RemoteAddr = "http://183.134.99.131:46789"
+var RemoteAddr = "http://58.211.191.14:45003"
 var UserNumber = 200
+var TimeNum = 10
 
 func main() {
 	flag.Parse()
@@ -31,11 +33,18 @@ func main() {
 		UserNumber = n
 	}
 
+	if *timeNum != "" {
+		n, err := strconv.Atoi(*timeNum)
+		if err != nil {
+			panic("the number of simulated users is error value")
+		}
+		TimeNum = n
+	}
 	for i := 0; i < UserNumber; i++ {
 		go Send(RemoteAddr)
 	}
 
-	time.Sleep(10 * time.Second)
+	time.Sleep(time.Duration(TimeNum) * time.Second)
 }
 
 func Send(remoteAddr string) {
@@ -44,15 +53,17 @@ func Send(remoteAddr string) {
 
 	client := swagger.NewAPIClient(config)
 
-	data := []swagger.ModelReqAutoProof{
-		{
-			Id: "1",
-			Ext: &swagger.ModelProofExtInfo{
-				TemplateId: 25,
-				Version:    "v4",
-			},
-			Detail: "\"{\\\"订单\\\":{\\\"订单id\\\":\\\"1\\\",\\\"下订单时间\\\":\\\"2009-01-01:12:00:00\\\",\\\"订单状态\\\":\\\"已完成\\\",\\\"交易编号\\\":\\\"001\\\",\\\"商品名称\\\":\\\"订单测试\\\",\\\"商品金额\\\":\\\"10\\\",\\\"商品数量\\\":\\\"1\\\",\\\"企业名称\\\":\\\"测试企业\\\",\\\"收货人姓名\\\":\\\"张三\\\",\\\"收货人地址\\\":\\\"浙江杭州\\\",\\\"收获人手机号\\\":\\\"13800000000\\\"}}\"",
+	dataInfo := swagger.ModelReqAutoProof{
+		Id: "1",
+		Ext: &swagger.ModelProofExtInfo{
+			TemplateId: 25,
+			Version:    "v4",
 		},
+		Detail: "\"{\\\"订单\\\":{\\\"订单id\\\":\\\"1\\\",\\\"下订单时间\\\":\\\"2009-01-01:12:00:00\\\",\\\"订单状态\\\":\\\"已完成\\\",\\\"交易编号\\\":\\\"001\\\",\\\"商品名称\\\":\\\"订单测试\\\",\\\"商品金额\\\":\\\"10\\\",\\\"商品数量\\\":\\\"1\\\",\\\"企业名称\\\":\\\"测试企业\\\",\\\"收货人姓名\\\":\\\"张三\\\",\\\"收货人地址\\\":\\\"浙江杭州\\\",\\\"收获人手机号\\\":\\\"13800000000\\\"}}\"",
+	}
+
+	data := []swagger.ModelReqAutoProof{
+		dataInfo,
 	}
 
 	signOpt := &swagger.ProofApiApiAutoProofsPostOpts{}
